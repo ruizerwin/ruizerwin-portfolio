d16014d0ff2a2645df1a8801b865b80803556b41
ERWIN D PADILLA <erwin@hayasolutions.com>
2026-04-14 13:59:30 -0400

    Updating nav content 1

--- includes/nav.php
+++ includes/nav.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 ?>
 <nav id="navmenu" class="navmenu">
     <ul>
-        <li><a href="#home" class="active" title="Home"><i class="bi bi-house navicon"></i><span>Home</span></a></li>
-        <li><a href="#about" title="About Me"><i class="bi bi-person navicon"></i><span>About Me</span></a></li>
-        <li><a href="#resume" title="Resume"><i class="bi bi-file-earmark-text navicon"></i><span>Resume</span></a></li>
-        <li><a href="#portfolio" title="Portfolio"><i class="bi bi-code-slash navicon"></i><span>Portfolio</span></a></li>
-        <li><a href="#services" title="Services"><i class="bi bi-gear navicon"></i><span>Services</span></a></li>
-        <li><a href="#contact" title="Contact"><i class="bi bi-envelope navicon"></i><span>Contact</span></a></li>
+        <li><a href="#home" class="active"><i class="bi bi-house navicon"></i><span>Home</span></a></li>
+        <li><a href="#about"><i class="bi bi-person navicon"></i><span>About</span></a></li>
+        <li><a href="#resume"><i class="bi bi-file-earmark-text navicon"></i><span>Resume</span></a></li>
+        <li><a href="#portfolio"><i class="bi bi-code-slash navicon"></i><span>Projects</span></a></li>
+        <li><a href="#services"><i class="bi bi-gear navicon"></i><span>Services</span></a></li>
+        <li><a href="#contact"><i class="bi bi-envelope navicon"></i><span>Contact</span></a></li>
     </ul>
 </nav>
